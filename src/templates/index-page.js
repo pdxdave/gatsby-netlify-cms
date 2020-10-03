92364fda5f14c07a05f275005ebdde207cb81438
pdxdave <webdevdave@hotmail.com>
2020-10-03 13:59:40 -0700

    title change

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -72,7 +72,7 @@ export const IndexPageTemplate = ({
               <div className="content">
                 <div className="content">
                   <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
+                    {/* <h1 className="title">{mainpitch.title}</h1> */}
                   </div>
                   <div className="tile">
                     <h3 className="subtitle">{mainpitch.description}</h3>
@@ -96,7 +96,7 @@ export const IndexPageTemplate = ({
                 </div>
                 <div className="column is-12">
                   <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
+                    Latest blogs
                   </h3>
                   <BlogRoll />
                   <div className="column is-12 has-text-centered">
