37e36b2ef36b5f1a2017c17f5b6b31bd6761a633
pdxdave <webdevdave@hotmail.com>
2020-10-03 12:58:30 -0700

    changed a link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,7 @@ import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
+import linkedin from '../img/social/linkedin.svg'
 import twitter from '../img/social/twitter.svg'
 import vimeo from '../img/social/vimeo.svg'
 
